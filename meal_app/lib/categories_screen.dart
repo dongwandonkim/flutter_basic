7b2bfb7a1771a1052b7fb5878ffdd8f954f15ef9
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-03-19 05:33:48 -0700

    category screen

--- meal_app/lib/categories_screen.dart
+++ meal_app/lib/categories_screen.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/category_item.dart';
+
+import './dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MealApp'),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map((data) => CategoryItem(title: data.title, color: data.color))
+            .toList(),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
-          mainAxisSpacing: 20),
+          mainAxisSpacing: 20,
+        ),
+      ),
     );
   }
 }
